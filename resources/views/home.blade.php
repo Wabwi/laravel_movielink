fd831c9d668ba0a0a4cce5828aed677bdccc91c7
Wabwi <wabwiibrahim@gmail.com>
2021-04-26 22:26:19 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,257 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                </div>
+
+
+
+                <section>
+                    <div class="">
+                        <div class="row row_1">
+
+                            <div class="col-sm-8 col-md-4">
+                                <div class="top_slider">
+                                    <h2>Trending Movies</h2>
+                                    <div id="carouselFull" class="carousel slide" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                                            <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                                            <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                                            <li data-target="#carouselIndicators" data-slide-to="3"></li>
+                                            <li data-target="#carouselIndicators" data-slide-to="4"></li>
+                                            <li data-target="#carouselIndicators" data-slide-to="5"></li>
+
+                                        </ol>
+
+                                        <!-- Wrapper for slides -->
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid"
+                                                    src="{{ url('../img/rim_of_the_world.jpg') }}">
+
+                                            </div>
+                                            <!--slider 1 -->
+
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid"
+                                                    src="{{ url('../../public/img/rim_of_the_world.jpg') }}">
+
+                                            </div>
+                                            <!--slider 2 -->
+
+
+
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid"
+                                                    src="{{ url('/img/rim_of_the_world.jpg') }}">
+
+                                            </div>
+                                            <!--slider 3-->
+
+
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid"
+                                                    src="{{ url('/img/rim_of_the_world.jpg') }}">
+
+                                            </div> <!-- slider 4 -->
+
+
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid"
+                                                    src="{{ url('/img/rim_of_the_world.jpg') }}">
+
+                                            </div> <!-- slider 5 -->
+
+
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid"
+                                                    src="{{ url('/img/rim_of_the_world.jpg') }}">
+
+                                            </div> <!-- slider 6 -->
+
+                                        </div> <!-- end of carousel-inner -->
+
+                                        <a href="#carouselFull" class="carousel-control-prev" role="button"
+                                            data-slide="prev"><span class="carousel-control-prev-icon"
+                                                aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span></a>
+
+
+                                        <a href="#carouselFull" class="carousel-control-next" role="button"
+                                            data-slide="next"><span class="carousel-control-next-icon"></span>
+                                            <span class="sr-only">Next</span></a>
+
+                                    </div>
+
+
+
+                                </div> <!-- end of div top_slider -->
+
+
+                            </div> <!-- end of div col-sm-8 -->
+
+
+                            <div class="col-sm-4 col-md-8">
+                                <div class="jumbotron-fluid jumbolist">
+                                    <h2>Other Websites</h2>
+                                    <hr>
+                                    <ul class="other_movies_list list-unstyled">
+
+                                        <li><a href="">Hdpopcorn</a></li>
+                                        <li><a href="">DivxCrawler</a></li>
+                                        <li><a href="">FMovies</a></li>
+                                        <li><a href="">Today Tv Series</a></li>
+                                        <li><a href="">We Watch</a></li>
+                                        <li><a href="">Cinema Home</a></li>
+                                        <li><a href="">All Movies</a></li>
+                                    </ul>
+
+
+                                    <a href="" class="btn btn-btn-default">View All</a>
+
+                                </div><!-- end of jumbotron tag -->
+
+                                <div class="most_downloaded">
+                                    <h2>Most Downloaded</h2>
+
+                                    <img src="img/the_informer.jpg" class="img-fluid">
+
+                                </div>
+
+                            </div> <!-- end of col-sm-4 tag-->
+
+
+
+                        </div>
+
+                    </div>
+
+
+                </section>
+
+
+
+                <section class="section banner">
+                    <div class="bg_overlay"></div>
+
+                    <div class="container">
+                        <div class="row">
+                            <?php if (isset($_SESSION['username'])) {
+                            $username = $_SESSION['username']; ?>
+                            <div class="col-sm-12 text-center text-white">
+                                <h3>Admin <?php echo $username; ?>!</h3>
+                                <p>You can now: add Movies<br>
+                                    get notified of new Movies<br>
+                                    view extra movies and features<br>
+                                    and be connected !
+
+                                </p>
+
+                            </div>
+
+                            <?php
+                            } else {
+                            ?>
+                            <div class="col-sm-12 text-center text-white">
+                                <h3>Create Account Now To Gain Admin Priviledges!</h3>
+                                <p>You can add Movies<br>
+                                    You will get notified of new Movies<br>
+                                    You can be the Admin
+
+                                </p>
+
+                            </div>
+                            <?php
+                            } ?>
+
+
+
+                        </div>
+
+                    </div>
+
+
+                </section>
+
+
+
+
+                <section class="movie_section">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="text-center">Movies</h2>
+
+
+                            <table class="table table-striped table-responsive-md">
+                                <thead>
+
+                                    <tr>
+                                        <th>Movie</th>
+                                        <th>&nbsp;</th>
+                                        <th>Rate</th>
+                                        <th>Downloads</th>
+                                        <th>&nbsp;</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+
+
+
+                                    <tr>
+                                        <td><a href="img/assimilate.jpg"><img src="img/assimilate.jpg" height="150"></a>
+                                        </td>
+                                        <td><strong>Assimilate:</strong><br>Action, <br>Thriller<br></td>
+                                        <td>9/10</td>
+                                        <td>43 Downloads</td>
+                                        <td><a href="" class="btn btn-primary">Download</a></td>
+
+                                    </tr>
+
+                                    <tr>
+                                        <td><a href="img/avengement.jpg"><img src="img/avengement.jpg" height="150"></a>
+                                        </td>
+                                        <td><strong>Avengement:</strong><br>Action, <br>Thriller<br></td>
+                                        <td>9/10</td>
+                                        <td>43 Downloads</td>
+                                        <td><a href="" class="btn btn-primary">Download</a></td>
+
+                                    </tr>
+
+                                    <tr>
+                                        <td><a href="img/the_poison_rose.jpg"><img src="img/the_poison_rose.jpg"
+                                                    height="150" class="movie_img"></a></td>
+                                        <td><strong>The Poison Rose:</strong><br>Thriller<br></td>
+                                        <td>9/10</td>
+                                        <td>43 Downloads</td>
+                                        <td><a href="" class="btn btn-primary">Download</a></td>
+
+                                    </tr>
+
+
+                                </tbody>
+
+
+                            </table>
+
+
+
+                        </div>
+
+                    </div> <!-- end row div -->
+
+                </section>
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>MovieLink</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+    <link rel="stylesheet" href="design/font-awesome/css/font-awesome.min.css">
+
+    {{-- <link rel="stylesheet" type="text/css" href="design/style.css"> --}}
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+
+    {{-- <script src="design/jquery.js"></script> --}}
+
+    <style type="text/css">
+        body {
+            margin: 0;
+            padding: 0;
+            background: #343a40;
+        }
+
+        .search_box {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: LightGray;
+            height: 40px;
+            border-radius: 40px;
+            padding: 5px;
+        }
+
+        .search_box:hover>.search_txt {
+            width: 200px;
+            padding: 0px 6px;
+        }
+
+
+        .search_box:hover>.search_btn {
+            background-color: white;
+        }
+
+        .search_btn {
+            color: LightGray;
+            float: right;
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            background: #2f3640;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.4s;
+        }
+
+        .search_txt {
+            border: none;
+            background: none;
+            outline: none;
+            float: left;
+            padding: 0;
+            color: white;
+            font-size: 16px;
+            transition: 0.4s;
+            line-height: 40px;
+            width: 0px;
+
+        }
+
+        .brand {
+            text-align: center;
+            text-decoration: none;
+            font-size: 2em;
+            font-weight: bolder;
+            padding-top: 50px;
+            margin-top: 100px;
+            color: white !important;
+
+        }
+
+        .brand a {
+            text-decoration: none;
+            color: white;
+        }
+
+        .link {
+            background-color: white;
+            color: black;
+            padding: 5px;
+            border-radius: 2px;
+
+        }
+
+        .movie {
+            font-family: Verdana;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .search_txt {
+                width: 150px;
+                padding-left: 4px;
+            }
+
+            .search_box:hover>.search_txt {
+                width: 105px;
+                padding: ;
+            }
+
+        }
+
+        @media only screen and (max-width: 450px) {
+            .search_box {
+                height: 35px;
+                border-radius: 35px;
+            }
+
+            .search_txt {
+                width: 90px;
+                padding-left: 4px;
+                line-height: 35px;
+                font-size: 13px;
+            }
+
+            .search_btn {
+                width: 35px;
+                height: 35px;
+
+            }
+
+            .search_box:hover>.search_txt {
+                width: 90px;
+
+            }
+
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-light underline">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-light underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-light underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+    </div>
+
+
+    <div class="brand">
+        <a href="{{ route('home') }}" class="navbar-brand"><span class="movie">Movie</span><span
+                class="link">Link</span></a>
+
+    </div>
+
+
+
+    <form method="POST" action="search_result.php">
+        <div class="search_box">
+            <input class="search_txt" type="text" name="search_movie" placeholder="Type to search">
+            <button type="submit" name="search_submit" class="search_btn">
+                <i class="fa fa-search"></i>
+
+            </button>
+
+        </div>
+
+    </form>
+
+
+
+</body>
+
+</html>
