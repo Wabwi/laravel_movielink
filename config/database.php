25904078fb4d21cf89fb0051a4fb8259e73e7c71
Wabwi <wabwiibrahim@gmail.com>
2021-04-29 22:12:51 +0300

    changed db config, changed readme

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('db4free.net'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'movie_link'),
+            'username' => env('DB_USERNAME', 'wabwire'),
+            'password' => env('DB_PASSWORD', 'jNzwUPAN9f43@UU'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
